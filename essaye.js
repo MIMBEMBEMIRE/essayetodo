020a20ad81192cb51530cd961e3b3045be7a3c31
unknown <miramimbembe1991@gmail.com>
2022-04-06 11:34:56 +0530

    switch statement

--- essaye.js
+++ essaye.js
@@ -1,33 +1,51 @@
+
 const express = require("express");
-const app=express();
 const bodyParser=require("express");
+const app=express();
 
 app.set("view engine", "ejs");
-
 
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res){
 
-    var today = new Date();
-    var day="";
-    var current =today.getDay();
-    if(today.current === 6 || today.current==0){
-           day ="weekday";
+var today = new Date();
 
-    }
-    else{
-       day="weeked";
-    }
-    res.render("essaye",{essaye: day});
-    
-    
+var current = today.getDay();
+var day="";
 
-});
+    switch(current){
+        case 0:
+            day ="sunday";
+            break;
+            case 1:
+                day="Monday";
+                break;
+                case 2:
+                day ="Twesday";
+            break;
+            case 3:
+            day="thurday"
+            break;
+            case 4:
+                day="Friday";
+                break;
+                case 5:
+                    day="surtarday";
+                    break;
+                    case 7:
+                    day="surday";
+                    break;
+                    default:
+    }  
+    res.render("list",{listday:day});
+
+
+                
+            });
+            
 
 app.listen(3000,function(){
-
-
 console.log("the server is running in server 300");
 
 });
